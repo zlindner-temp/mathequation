daabb403ba4186b8306f45e2dd3b4ec1c7e12491
lindz0150 <lindz0150@FHC-IC0037270.wrdsb.ca>
2015-04-08 13:28:19 -0400

    initial

---
+++ src/mathequation.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lindz0150
+ */
+public class mathequation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
