8c8a950a9751516b5236c0ba04e7edc6e2fabb7a
lindz0150 <lindz0150@FHC-IC0037270.wrdsb.ca>
2015-04-08 13:39:33 -0400

    mawky

--- mathequation/src/mathequation/Mathequation.java
+++ mathequation/src/mathequation/Mathequation.java
@@ -4,9 +4,9 @@ public class Mathequation {
 
     public static void main(String[] args) {
         
-        // Chuckie: I have -10/
+        // Chuckie: -10/
         
-        // Ariana: I have 5
+        // Ariana: 5
         
         //Zach: *4-
         
