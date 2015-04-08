7455d77b2c1dff03ae6ea18633f73daf93f60968
lindz0150 <lindz0150@FHC-IC0037270.wrdsb.ca>
2015-04-08 13:38:43 -0400

    marcus weher r u

--- mathequation/src/mathequation/Mathequation.java
+++ mathequation/src/mathequation/Mathequation.java
@@ -1,26 +1,14 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package mathequation;
 
-/**
- *
- * @author dinht6168
- */
 public class Mathequation {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         // Chuckie: I have -10/
         
         // Ariana: I have 5
         
         //Zach: *4-
         
-        //Chuckie: What does hitler have?
     }
 }
