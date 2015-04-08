7b6ebaeb862542a669e359f1a7e5213cc6bcf07b
dinht6168 <dinht6168@FHC-IC0037266.wrdsb.ca>
2015-04-08 13:31:19 -0400

    I have -10/

---
+++ mathequation/src/mathequation/Mathequation.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathequation;
+
+/**
+ *
+ * @author dinht6168
+ */
+public class Mathequation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Chuckie: I have -10/
+    }
+}
